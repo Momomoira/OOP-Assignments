d8f4ad07c072be0f59c9b9b27fcfc5e0dc3622d3
Momo <mccallangan2023@plm.edu.ph>
2025-06-28 21:00:16 +0800

    Add files via upload

---
+++ LabAssignment3/OverrideFunction.java
@@ -0,0 +1,11 @@
+package oop_activity4;
+
+/**
+ *
+ * @author Dela Rosa
+ */
+public class OverrideFunction {
+    public int area(int intNum1, int intNum2) {
+        return intNum1 % intNum2;
+    }
+}
