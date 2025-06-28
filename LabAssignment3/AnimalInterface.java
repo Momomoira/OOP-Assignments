2effacd99dd856f3c8b16320fb62e29d03566b52
Momo <mccallangan2023@plm.edu.ph>
2025-06-28 21:00:42 +0800

    Add files via upload

---
+++ LabAssignment3/AnimalInterface.java
@@ -0,0 +1,14 @@
+
+package oop_activity4;
+
+/**
+ *
+ * @author Dela Rosa
+ */
+public interface AnimalInterface {
+ 
+    void animalSound(String strKind);
+    void animalSkin(String strKind);
+    int animalAge(int intAge);
+    void animalFeet(String strKind);
+}
